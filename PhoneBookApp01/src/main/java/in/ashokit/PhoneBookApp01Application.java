299e91f33e634cc5b58538d77d0d36f3360e5cbf
siri <siri@DESKTOP-AMC64JR>
2021-12-26 19:20:41 +0530

    First commit

---
+++ PhoneBookApp01/src/main/java/in/ashokit/PhoneBookApp01Application.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApp01Application.class, args);
+	}
+
+}
---
+++ PhoneBookApp01/src/test/java/in/ashokit/PhoneBookApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
