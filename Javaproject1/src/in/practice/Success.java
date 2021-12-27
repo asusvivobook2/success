55f460671754dfbf5b330e255043e779e920a9e7
siri <siri@DESKTOP-AMC64JR>
2021-12-27 10:31:42 +0530

    java proj Commit

---
+++ Javaproject1/src/in/practice/Success.java
@@ -0,0 +1,12 @@
+package in.practice;
+
+public class Success {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Success Executed Successfully");
+		// TODO Auto-generated method stub
+
+	}
+
+}
