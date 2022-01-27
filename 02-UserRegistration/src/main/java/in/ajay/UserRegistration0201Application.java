2d87b91673c60456ed1009be252d50ccedc0c5e3
siri <siri@DESKTOP-AMC64JR>
2022-01-27 14:52:52 +0530

    Dev Done

--- 02-UserRegistration/src/main/java/in/ajay/UserRegistration0201Application.java
+++ 02-UserRegistration/src/main/java/in/ajay/UserRegistration0201Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserRegistration0201Application {
+public class 02UserRegistrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserRegistration0201Application.class, args);
+		SpringApplication.run(02UserRegistrationApplication.class, args);
 	}
 
 }
